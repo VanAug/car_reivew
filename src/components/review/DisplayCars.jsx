8387429be4a4f7194285220b4a0cce35f6bfecf5
Brian <okellobrian18@gmail.com>
2025-04-23 19:54:20 +0300

    Displaying Cars

--- src/components/review/DisplayCars.jsx
+++ src/components/review/DisplayCars.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import Car from '../Car/Car'
-import Search from '../Search/Search'
+import React, { useEffect, useState } from 'react';
+import Search from '../Search/Search';
+import CarCard from '../Car/CarCard';
 
 const DisplayCars = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    console.clear();
+    fetch('http://localhost:3000/cars')
+      .then(res => {
+        if (!res.ok) throw new Error('Failed to fetch cars');
+        return res.json();
+      })
+      .then(data => {
+        setCars(data.cars || data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div>
       <Search />
-      <Car />
+      <h2>Main Display</h2>
+      <div className="card-container">
+        {cars.length === 0 ? (
+          <p>Loading cars...</p>
+        ) : (
+          cars.map((car, index) => (
+            <CarCard key={index} car={car} />
+          ))
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayCars
+export default DisplayCars;

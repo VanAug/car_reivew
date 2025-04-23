99ded4adcdd907213a0a5ff1c352c9a49a1c8bc7
Mati Joseph <crucialniccur@gmail.com>
2025-04-23 12:17:46 +0300

    paste in the nav code into NavBar.jsx

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import "./Navbar.css";
 import AppRoutes from "../../routes";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/" className="logo">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/">Review</a>
+          </li>
+          <li>
+            <a href="/">Favorites</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default NavBar;

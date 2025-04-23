1dd0ec3c407739c5d3ad1f3969d4e08962841549
Brian <okellobrian18@gmail.com>
2025-04-23 14:54:19 +0300

    Search Location.

--- src/components/review/DisplayCars.jsx
+++ src/components/review/DisplayCars.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Search from '../Search/Search'
 
 const DisplayCars = () => {
   return (
-    <div>DisplayCars</div>
+    <div>
+      <Search />
+    </div>
   )
 }
 

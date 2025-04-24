f3822b9090ecc92b739dcbc5321597d438a50989
Mati Joseph <crucialniccur@gmail.com>
2025-04-24 11:14:11 +0300

    add a active to highlight when the page is currently rendered

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-import AppRoutes from "../../routes";
 
 const NavBar = () => {
   return (
     <>
       <nav>
         <div className="logo">
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "nav-active" : "")}
+          >
+            Home
+          </NavLink>
         </div>
         <ul className="nav-links">
           <li>
-            <NavLink to="/display">Display</NavLink>
+            <NavLink
+              to="/display"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Display
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/favorites">Favorites</NavLink>
+            <NavLink
+              to="/favorites"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Favorites
+            </NavLink>
           </li>
         </ul>
       </nav>

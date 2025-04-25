4a90fcfc17f001093e44a2a2ccf33709e34495ad
Mati Joseph <crucialniccur@gmail.com>
2025-04-25 08:51:55 +0300

    add a search icon and wrap in a div, and add classNames for styling

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,13 +3,16 @@ import "./search.css";
 
 const Search = ({ handleSearch }) => {
   return (
-    <div style={{ marginBottom: "20px" }} className="search-container">
-      <input
-        type="text"
-        placeholder="Search for a car..."
-        onChange={(e) => handleSearch(e.target.value)}
-        className="search-input"
-      />
+    <div className="search-container">
+      <div class="search-wrap">
+        <span className="search-icon">🔍</span>
+        <input
+          type="text"
+          placeholder="Search for a car..."
+          onChange={(e) => handleSearch(e.target.value)}
+          className="search-input"
+        />
+      </div>
     </div>
   );
 };

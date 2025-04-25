41ead22206355c25a6bd03c9abe334aa4e254387
Mati Joseph <crucialniccur@gmail.com>
2025-04-25 12:50:49 +0300

    add nav link with routes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,6 +9,13 @@ const Footer = () => {
           <h2>Car rev innit.</h2>
           <p>We all love cars ama namna gani.</p>
         </div>
+        <div class="footer-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/display">Display</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
+          <NavLink to="/signin">Sign in </NavLink>
+          <NavLink to="/signup">Sign up</NavLink>
+        </div>
       </div>
     </footer>
   );

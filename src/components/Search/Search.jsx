35785a77a7139ea39e6700264c0fea8ed245ab58
Mati Joseph <crucialniccur@gmail.com>
2025-04-25 08:29:54 +0300

    import the search css file into Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./search.css";
 
 const Search = ({ handleSearch }) => {
-  
   return (
-    <div style={{ marginBottom: '20px' }}>
-      <input 
-        type="text" 
-        placeholder="Search for a car..." 
-        onChange={(e) => handleSearch(e.target.value)} 
-        style={{ padding: '8px', width: '300px', fontSize: '16px' }}
+    <div style={{ marginBottom: "20px" }}>
+      <input
+        type="text"
+        placeholder="Search for a car..."
+        onChange={(e) => handleSearch(e.target.value)}
+        style={{ padding: "8px", width: "300px", fontSize: "16px" }}
       />
     </div>
   );

fab95696394e23949adc1805fd3d765a44bbc5d5
Mati Joseph <crucialniccur@gmail.com>
2025-04-25 21:58:23 +0300

    remove NavLinks on the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,13 +10,7 @@ const Footer = () => {
           <h2>Car rev innit.</h2>
           <p>We all love cars ama namna gani.</p>
         </div>
-        <div class="footer-links">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/display">Display</NavLink>
-          <NavLink to="/favorites">Favorites</NavLink>
-          <NavLink to="/signin">Sign in </NavLink>
-          <NavLink to="/signup">Sign up</NavLink>
-        </div>
+
         <div className="footer-bottom">
           <p>
             &copy; {new Date().getFullYear()} Rev Ride. All rights reserved.

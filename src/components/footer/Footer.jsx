761f6422989da2e8ea61191fc3aa028201b30e9b
Mati Joseph <crucialniccur@gmail.com>
2025-04-25 12:48:11 +0300

    add footer h2 and p

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer innit</div>;
+  return (
+    <footer className="footer">
+      <div class="footer-container">
+        <div class="footer-brand">
+          <h2>Car rev innit.</h2>
+          <p>We all love cars ama namna gani.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

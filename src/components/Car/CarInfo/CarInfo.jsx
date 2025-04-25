fc20ff9a513bc701d5f521ac17f45b771c1ee70b
Brian <okellobrian18@gmail.com>
2025-04-25 17:14:29 +0300

    car info css

--- src/components/Car/CarInfo/CarInfo.jsx
+++ src/components/Car/CarInfo/CarInfo.jsx
@@ -20,7 +20,6 @@ const CarInfo = () => {
   return (
     <div className="carinfo-container">
       <h2 className='infoTitle'>More on the {car.name}</h2>
-      <h1 className="carinfo-title">{car.name}</h1>
       <img src={car.image} alt={car.name} className="carinfo-image" />
       <p className="carinfo-year">{car.year}</p>
       <p className="carinfo-info">{car.info}</p>
